1847c21a4e0bae2b97b2788f69215915be7f120d
Antonio León <antonioleoncarrillo@gmail.com>
2015-05-10 10:57:39 +0200

    Añadir nuevos requirimientos

--- fsworld/venv/Scripts/activate.bat
+++ fsworld/venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Antonio-PC\Desktop\TFG\fsworld\venv"
+set "VIRTUAL_ENV=C:\Users\AntPc\Documents\testing2\fsworld\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
---
+++ fsworld/venv/Scripts/static-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\AntPc\Documents\testing2\fsworld\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'static==1.1.1','console_scripts','static'
+__requires__ = 'static==1.1.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('static==1.1.1', 'console_scripts', 'static')()
+    )
